e2eb5fcb7adaab917c08b7fa8411f7d8adbde893
mikeyQwn <mikeyQwn@gmail.com>
2024-04-05 12:36:12 +0300

    Add alacritty config

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 rm -rf ./nvim
+rm -rf ./alacritty
 rm -rf ./.tmux.conf
 rm -rf ./.bashrc
 rm -rf ./.zshrc
--- copy.sh
+++ copy.sh
@@ -1,4 +1,5 @@
 cp -r ~/.config/nvim ./
+cp -r ~/.config/alacritty ./
 cp -r ~/.tmux.conf ./
 cp -r ~/.bashrc ./
 cp -r ~/.zshrc ./
